d15af2ed86161bfb6d6070a04f20b79bfd20ac12
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 22:07:09 +0000

    Adding tarfile member sanitization to extractall()

--- middleware/src/middlewared/middlewared/plugins/config.py
+++ middleware/src/middlewared/middlewared/plugins/config.py
@@ -122,7 +122,26 @@ class ConfigService(Service):
                 with tarfile.open(config_file_name) as tar:
                     bundle = True
                     tmpdir = tempfile.mkdtemp(dir='/var/tmp/firmware')
-                    tar.extractall(path=tmpdir)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar, path=tmpdir)
                     config_file_name = os.path.join(tmpdir, 'freenas-v1.db')
             except tarfile.ReadError:
                 bundle = False
